828ef2d45d7ed1a9f736617851dade7aabcb577f
parvezjilani <parvezjilani@gmail.com>
2021-11-02 18:56:37 +0530

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+ var numberofdrum = document.querySelectorAll(".drum");
+
+ for( var i=0; i<=numberofdrum.length;i++){
+ document.querySelectorAll("button")[i].addEventListener("click",function(){
+ alert("i got clicked")}
+ );
+ }
+
+// var audio = new Audio("tom-1.mp3")
+// var audio2 = new Audio("tom-2.mp3")
+// var audio3 = new Audio("tom-3.mp3")
+// var audio4 = new Audio("tom-4.mp3")
+// var audio5 = new Audio("snare.mp3")
+// var audio6 = new Audio("kick-bass.mp3")
+// var audio7 = new Audio("crash.mp3")
+// var drumAudio = document.querySelectorAll(".drum")
+// var btns = document.querySelectorAll("button")
+// var btnsss = document.querySelector("#asdfg")
+
+// for (i =i; i<=drumAudio.length; i++) {
+//     btns[i].addEventListener("click",(e)=>{
+//         const audioplayer =e.currentTarget.classList;
+//         if (audioplayer.contains("Sa")) {
+//             audio.play()
+//         }
+//        else if (audioplayer.contains("Re")) {
+//             audio2.play()
+//         }
+//        else if (audioplayer.contains("Ga")) {
+//             audio3.play()
+//         }
+//         else if (audioplayer.contains("Ma")) {
+//             audio4.play()
+//         }
+//         else if (audioplayer.contains("Pa")) {
+//             audio5.play()
+//         }
+//         else if (audioplayer.contains("Dha")) {
+//             audio6.play()
+//         }
+//         else if (audioplayer.contains("Ni")) {
+//             audio7.play()
+//         }
+
+//     });
+//    
+    
+
+    
+    
